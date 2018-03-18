5bd8b16b73c4551ee74e7f34fac077919e3d3655
MaxiReglisse <ernest.chiarello@univ-fcomte.fr>
2018-03-18 18:55:51 +0100

    path_log and not log_path

--- geosync/geosync/bin/sync_owncloud_data.sh
+++ geosync/geosync/bin/sync_owncloud_data.sh
@@ -14,10 +14,10 @@ source "$paramfile"
 if [[ "${share_directory}" ]]; then 
     INPUT_COPY_PATH="${share_directory}"
 else
-    echo "WARNING aucun chemin d'arborescence à publier ('share_directory') défini dans .geosync.conf" >> $LOG_PATH/publish_error.log
+    echo "WARNING aucun chemin d'arborescence à publier ('share_directory') défini dans .geosync.conf" >> $PATH_LOG/publish_error.log
 
     INPUT_COPY_PATH="$HOME/owncloudsync" # le chemin par défaut est conservé temporairement pour rétro-compatibilité # TODO ne pas prendre de valeur pas défaut et faire une vraie erreur
-    echo "WARNING chemin d'arborescence par défaut : ${INPUT_COPY_PATH}"  >> $LOG_PATH/publish_error.log
+    echo "WARNING chemin d'arborescence par défaut : ${INPUT_COPY_PATH}"  >> $PATH_LOG/publish_error.log
 fi
 
 verbose=1 # commenter pour diminuer les logs
